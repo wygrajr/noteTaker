f5b7ae5365291b5505b9bcbbea2e21b640e226bd
wygrajr <wycliffe.graftonjr@gmail.com>
2023-08-12 16:30:05 -0400

    Added uuid bolierplate helper and notes POST request

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils.js');
+const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the tips
 notes.get('/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-
+// POST Route for a new UX/UI tip
+notes.post('/notes', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        notes_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
+  
 module.exports = notes;
